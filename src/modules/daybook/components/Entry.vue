ed75bccf1446004d1240b0c384952718268768d5
Jose Astorayme <jose.astorayme@pwc.com>
2022-09-27 19:57:09 -0500

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2" @click="$router.push({name: 'entry', params: { id: 10 }})">
+    <div class="entry-title d-flex">
+        <span class="text-success fs-5 fw-bold">15</span>
+        <span class="mx-1 fs-5">Julio</span>
+        <span class="mx-2 fw-light">2021, jueves</span>
+    </div>
+
+    <div class="entry-description">
+        Ex ex aute qui incididunt adipisicing. Ad minim officia excepteur sit aute pariatur. Quis et nostrud deserunt ad cillum nisi occaecat proident magna voluptate nostrud consequat labore aliqua. Consequat Lorem excepteur nostrud eiusmod dolor velit ea nisi magna occaecat ex laboris elit. Amet velit fugiat occaecat magna excepteur duis non adipisicing.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+
+
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), //1234556544
+            date: new Date().toDateString(), // sat 23, Julio
+            text: 'Duis voluptate fugiat labore fugiat tempor commodo velit sit incididunt do dolore. Tempor do adipisicing sunt consectetur sunt voluptate commodo dolor laborum. Elit ea commodo Lorem aliqua consequat aliquip enim laborum sint ea tempor minim sint commodo. Anim fugiat aliquip aute amet ea occaecat ea ullamco sint culpa Lorem. Minim sunt occaecat laboris ea ut labore mollit ipsum commodo ea est qui enim. Proident culpa do commodo Lorem veniam aliquip.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000, //1234556544
+            date: new Date().toDateString(), // sat 23, Julio
+            text: 'Nisi do incididunt voluptate non pariatur. Commodo adipisicing duis nostrud ea qui. Nisi tempor cupidatat ullamco exercitation officia commodo amet velit sunt ut duis magna. Adipisicing quis culpa aliquip nisi nisi qui. Duis dolor et magna quis ex nulla non elit aliqua velit officia deserunt deserunt eu. Ex consectetur ea exercitation ea ut reprehenderit ex anim minim officia quis in et. Ut quis aliqua do do Lorem labore sit culpa.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000, //1234556544
+            date: new Date().toDateString(), // sat 23, Julio
+            text: 'Incididunt Lorem nostrud est veniam ullamco excepteur dolore est laborum elit ea. Id sint elit aliquip esse mollit commodo cillum culpa. Dolore velit sint tempor voluptate eu qui ipsum cillum ipsum id velit dolore. Tempor elit sit qui occaecat ipsum consectetur consequat qui id velit incididunt amet. Officia nisi Lorem proident adipisicing consectetur sunt culpa reprehenderit et aute incididunt tempor enim occaecat.',
+            picture: null,
+        },
+    ]
+})
